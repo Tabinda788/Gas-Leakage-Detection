60ecfbac9f3aac0884cda0b536b63369b4481422
Tabinda788 <tabindabhat21@gmail.com>
2021-12-24 15:11:03 +0530

    Adding gas leakage code

---
+++ WriteSingleField/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "YourSSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "YourPassword"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID yourchannelnumber			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "APIKEY"   // replace XYZ with your channel write API Key
